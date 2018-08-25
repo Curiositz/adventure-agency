c801c123526986a72fbd58e12b559bc67813c01a
Curiosityy <voujum@gmail.com>
2018-08-25 22:40:35 +0200

    Automatic Sprite Generation.

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+svgSprite = require('gulp-svg-sprite')
+rename = require('gulp-rename'),
+del = require('del');
+
+var config = {
+	mode: {
+		css: {
+			sprite: 'sprite.svg',
+			render: {
+				css: {
+					template: './gulp/templates/sprite.css'
+				}
+			}
+		}
+	}
+}
+
+
+/* Cleans Old Sprites*/
+gulp.task('beginClean', function() {
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+/* Generates Sprite */
+gulp.task('createSprite', ['beginClean'], function() {
+	return gulp.src('./app/assets/images/icons/**/*.svg')
+		.pipe(svgSprite(config))
+		.pipe(gulp.dest('./app/temp/sprite'));
+});
+
+/* Moves Graphics */
+gulp.task('copySpriteGraphic', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/**/*.svg')
+		.pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+/* Moves CSS */
+gulp.task('copySpriteCSS', ['createSprite'], function() {
+	return gulp.src('./app/temp/sprite/css/*.css')
+		.pipe(rename('_sprite.css'))
+		.pipe(gulp.dest('./app/assets/styles/modules'));
+}) ;
+
+/* Removes Initial Sprite */
+gulp.task('endClean', ['copySpriteCSS', 'copySpriteGraphic'], function() {
+	return del('./app/temp/sprite');
+});
+
+gulp.task('icons', [ 'beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
